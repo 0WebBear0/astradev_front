bdd5986124494ff35ea73681357f78e324236491
Алиев Ислам <salamvsem4567@gmail.com>
2022-11-27 12:41:56 +0300

    add habr and hh

--- src/pages/HHPage.vue
+++ src/pages/HHPage.vue
@@ -4,45 +4,81 @@
     <div class="hh_container">
       <div class="hh_item">
         <div class="hh_itemTop">
-          <div class="hh_avatar">
-            <img src="" alt="">
+          <div class="hh_itemTopLeft">
+            <div class="hh_name">Ключевое слово</div>
           </div>
         </div>
-        <div>Ключевое слово</div>
-        <div>Количество вакансий:</div>
-        <div>Максимальная зарплата</div>
-        <div>Минимальная зарплата</div>
-        <div>Средняя зарплата</div>
+        <div class="hh_itemBody">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit facilis.
+        </div>
+      </div>
+      <div class="hh_item">
+        <div class="hh_itemTop">
+          <div class="hh_itemTopLeft">
+            <div class="hh_name">Ключевое слово</div>
+          </div>
+        </div>
+        <div class="hh_itemBody">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit facilis.
+        </div>
+      </div>
+      <div class="hh_item">
+        <div class="hh_itemTop">
+          <div class="hh_itemTopLeft">
+            <div class="hh_name">Ключевое слово</div>
+          </div>
+        </div>
+        <div class="hh_itemBody">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit facilis.
+        </div>
+      </div>
+      <div class="hh_item">
+        <div class="hh_itemTop">
+          <div class="hh_itemTopLeft">
+            <div class="hh_name">Ключевое слово</div>
+          </div>
+        </div>
+        <div class="hh_itemBody">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit facilis.
+        </div>
+      </div>
+      <div class="hh_item">
+        <div class="hh_itemTop">
+          <div class="hh_itemTopLeft">
+            <div class="hh_name">Ключевое слово</div>
+          </div>
+        </div>
+        <div class="hh_itemBody">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit facilis.
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
-import {apiMy} from "../boot/axios";
+import { apiMy } from "../boot/axios";
 
 export default {
   name: "HHPage",
   data() {
     return {
-      response : null,
+      response: null,
       hhItems: null,
-    }
+    };
   },
   mounted() {
-    apiMy.get("api/keywords/hh?name=pidorasss")
-      .then(
-        (res)=> {
-          console.log(res)
-          this.response = res.data
-        }
-      )
-      .catch((err)=>{
-        console.log(err)
+    apiMy
+      .get("api/keywords/hh?name=pidorasss")
+      .then((res) => {
+        console.log(res);
+        this.response = res.data;
       })
-  }
-}
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
 </script>
 
 <style lang="scss">
@@ -54,17 +90,76 @@ export default {
     gap: 1.5rem;
   }
   &_item {
-    min-width: 50vw;
+    min-width: 70vw;
+    max-width: 70vw;
     border-radius: 12px;
     padding: 1rem 1.5rem;
-    color: #fff;
+    color: #000;
     background-color: #f4f3ff56;
-    box-shadow: 7px 6px 3px #cff8c9;
-    background-image: linear-gradient(-30deg, #1f0cca, #37c627 0%, #194fe4);
+    box-shadow: 7px 6px 3px #b2dfab;
+    &:last-child {
+      margin-bottom: 5rem;
+    }
+    &:nth-child(2n) {
+      justify-self: flex-end;
+      margin-left: auto;
+    }
   }
   &_itemTop {
     display: flex;
     justify-content: space-between;
+  }
+  &_itemTopLeft {
+    display: flex;
+    gap: 20px;
+    align-items: center;
+    font-size: 18px;
+    font-weight: 500;
+  }
+  &_avatar {
+    border-radius: 50%;
+    min-width: 50px;
+    min-height: 50px;
+    max-width: 50px;
+    max-height: 50px;
+    background-image: linear-gradient(-30deg, #1f0cca, #37c627 0%, #194fe4);
+  }
+  &_dots {
+    font-size: 30px;
+    line-height: 20px;
+    cursor: pointer;
+  }
+  &_itemBody {
+    margin-top: 10px;
+  }
+  &_itemBottom {
+    margin-top: 15px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  &_views {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+  &_reactions {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+  &_like {
+    cursor: pointer;
+    img {
+      max-width: 25px;
+      transform: rotate(180deg);
+    }
+  }
+  &_dislike {
+    cursor: pointer;
+    img {
+      max-width: 25px;
+    }
   }
 }
 </style>
--- src/pages/HabrPage.vue
+++ src/pages/HabrPage.vue
@@ -1,13 +1,147 @@
 <template>
-
+  <div>
+    <div class="habr_container">
+      <div class="habr_item">
+        <div class="habr_itemTop">
+          <div class="habr_itemTopLeft">
+            <div class="habr_avatar"></div>
+            <div class="habr_name">Ключевое слово</div>
+          </div>
+          <div class="habr_itemTopRight">
+            <div class="habr_dots">…</div>
+          </div>
+        </div>
+        <div class="habr_itemBody">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non aliquam
+          eaque dicta id similique asperiores distinctio accusantium labore
+          voluptate, quae sint neque laborum adipisci temporibus! In tempora
+          commodi voluptate facilis. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Non aliquam eaque dicta id similique asperiores
+          distinctio accusantium labore voluptate, quae sint neque laborum
+          adipisci temporibus! In tempora commodi voluptate facilis. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Non aliquam eaque dicta
+          id similique asperiores distinctio accusantium labore voluptate, quae
+          sint neque laborum adipisci temporibus! In tempora commodi voluptate
+          facilis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+          aliquam eaque dicta id similique asperiores distinctio accusantium
+          labore voluptate, quae sint neque laborum adipisci temporibus! In
+          tempora commodi voluptate facilis.
+        </div>
+        <div class="habr_itemBottom">
+          <div class="habr_reactions">
+            <div class="habr_like">
+              <img src="../assets/icons/like.svg" alt="" />
+            </div>
+            <div class="habr_dislike">
+              <img src="../assets/icons/like.svg" alt="" />
+            </div>
+          </div>
+          <div class="habr_views">
+            <img src="../assets/icons/eye.svg" alt="" /> Просмотров: 0
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { apiMy } from "../boot/axios";
+
 export default {
-  name: "HabrPage"
-}
+  name: "HabrPage",
+  data() {
+    return {
+      response: null,
+      habrItems: null,
+    };
+  },
+  mounted() {
+    apiMy
+      .get("api/keywords/habr?name=pidorasss")
+      .then((res) => {
+        console.log(res);
+        this.response = res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
 </script>
 
-<style scoped>
-
+<style lang="scss">
+.habr {
+  &_container {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    gap: 1.5rem;
+  }
+  &_item {
+    min-width: 50vw;
+    max-width: 50vw;
+    border-radius: 12px;
+    padding: 1rem 1.5rem;
+    color: #000;
+    background-color: #f4f3ff56;
+    box-shadow: 7px 6px 3px #b2dfab;
+  }
+  &_itemTop {
+    display: flex;
+    justify-content: space-between;
+  }
+  &_itemTopLeft {
+    display: flex;
+    gap: 20px;
+    align-items: center;
+    font-size: 18px;
+    font-weight: 500;
+  }
+  &_avatar {
+    border-radius: 50%;
+    min-width: 50px;
+    min-height: 50px;
+    max-width: 50px;
+    max-height: 50px;
+    background-image: linear-gradient(-30deg, #1f0cca, #37c627 0%, #194fe4);
+  }
+  &_dots {
+    font-size: 30px;
+    line-height: 20px;
+    cursor: pointer;
+  }
+  &_itemBody {
+    margin-top: 10px;
+  }
+  &_itemBottom {
+    margin-top: 15px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  &_views {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+  &_reactions {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+  &_like {
+    cursor: pointer;
+    img {
+      max-width: 25px;
+      transform: rotate(180deg);
+    }
+  }
+  &_dislike {
+    cursor: pointer;
+    img {
+      max-width: 25px;
+    }
+  }
+}
 </style>
